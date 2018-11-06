fa2d13298525a7c9a18effc0c5fd96b9fc4c8651
Felix Niemeyer <niemeyer.felix@gmail.com>
2018-11-06 15:59:54 +0100

    pivot

---
+++ db.js
@@ -0,0 +1,30 @@
+class DB {
+  async searchAims() {
+  }
+/* featureitis
+  async getIdentity() {
+  }
+  async getFocussedIdeaId() {
+  }
+  async focusIdea() {
+  }
+  async searchIdea() {
+  }
+  async getNearbyIdeasAndConnections() {
+  }
+  async addIdea() {
+  }
+  async addConnection() {
+  }
+  async updateIdea() {
+  }
+  async updateConnection() {
+  }
+  async rmIdea() {
+  }
+  async rmConnection() {
+  }
+*/
+}
+  
+module.exports = DB
